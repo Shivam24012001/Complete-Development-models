aaa4858f6f1591ea453df4383d3bccc1ea019f14
Shivam Mishra <smvns9119@gmail.com>
2025-07-30 21:34:50 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,5 @@
 import sys
-from logger import logging
-
+from src.logger import logging   # Correct import path
 
 def error_message_detail(error, error_detail):
     _, _, exc_tb = error_detail.exc_info()
@@ -9,12 +8,11 @@ def error_message_detail(error, error_detail):
     return "Error occurred in Python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name, line_number, str(error)
     )
-    
-    
+
 class CustomException(Exception):
-        def __init__(self, error_message, error_detail):
-            super().__init__(error_message)
-            self.error_message = error_message_detail(error_message, error_detail)
-        def __str__(self):
-            return self.error_message
+    def __init__(self, error_message, error_detail):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
         
+    def __str__(self):
+        return self.error_message
